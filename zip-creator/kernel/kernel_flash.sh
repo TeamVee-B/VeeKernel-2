8659265d0be14add71082b1743c92971f6006454
Caio99BR <caiooliveirafarias0@gmail.com>
2015-06-11 07:57:09 -0300

    Kernel build Script Revision 9

--- zip-creator/kernel/kernel_flash.sh
+++ zip-creator/kernel/kernel_flash.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 cd /tmp/
-/tmp/dd if=/dev/block/mmcblk0p9 of=/tmp/boot.img
+dd if=/dev/block/mmcblk0p9 of=/tmp/boot.img
 /tmp/unpackbootimg /tmp/boot.img
-/tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline 'androidboot.hardware=m4 lge.signed_image=false' --base 0x00200000  --ramdiskaddr 0x01400000 -o /tmp/newboot.img
-/tmp/dd if=/tmp/newboot.img of=/dev/block/mmcblk0p9
+/tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline 'androidboot.hardware=vee3 androidboot.selinux=permissive' --base 0x00200000  --ramdiskaddr 0x01500000 -o /tmp/newboot.img
+dd if=/tmp/newboot.img of=/dev/block/mmcblk0p9
 busybox chmod 644 /system/lib/modules/*
